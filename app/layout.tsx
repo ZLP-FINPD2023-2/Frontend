b40fdc4c076612b2adc31f2da9894e42f8f7970a
DimDimich-creator <stilys62@gmail.com>
2023-10-16 15:37:39 +0300

    fix(pwa): fix PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,8 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "black" },
   ],
   icons: {
-    icon: "/favicon.ico",
+    icon: "/icon.svg",
+    // favicon: "/favicon.ico",
     shortcut: "/favicon-16x16.png",
     apple: "/apple-touch-icon.png",
   },
